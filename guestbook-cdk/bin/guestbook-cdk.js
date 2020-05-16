cb9348434885b1a2fe6713b3baf1298573506d0a
Bashar <alfallouji@gmail.com>
2020-05-16 17:32:59 +0000

    update

--- guestbook-cdk/bin/guestbook-cdk.js
+++ guestbook-cdk/bin/guestbook-cdk.js
@@ -2,7 +2,23 @@
 
 const cdk = require('@aws-cdk/core');
 const { GuestbookVpcStack } = require('../lib/guestbook-vpc-stack');
-const props = require('../conf/config.json');
 
 const app = new cdk.App();
-new GuestbookVpcStack(app, 'GuestbookVpcStack', props);
+
+const env_name = app.node.tryGetContext('env');
+switch(env_name) { 
+    case 'prod':
+        var props = require('../conf/config.prod.json');
+    break;
+    
+    case 'dev':
+        var props = require('../conf/config.dev.json');
+    break;
+    
+    default:
+        throw('An environment context must be provided (-c env=prod|dev)');
+    break;
+}
+
+var prefix = env_name.charAt(0).toUpperCase() + env_name.slice(1);
+new GuestbookVpcStack(app, 'Guestbook' + prefix + 'VpcStack', props);
--- guestbook-cdk/lib/guestbook-vpc-stack.js
+++ guestbook-cdk/lib/guestbook-vpc-stack.js
@@ -13,26 +13,26 @@ class GuestbookVpcStack extends cdk.Stack {
 
     // Create a VPC
     const vpc = new ec2.Vpc(this, props.vpc.name, {
-      maxAzs: 3,
-     	subnetConfiguration: [
-           {
-             cidrMask: 24,
-             name: 'public',
-             subnetType: ec2.SubnetType.PUBLIC,
-           },
-           {
-             cidrMask: 24,
-             name: 'private',
-             subnetType: ec2.SubnetType.PRIVATE,
-           },
-           {
-             cidrMask: 24,
-             name: 'isolated',
-             subnetType: ec2.SubnetType.ISOLATED,
-           }
+        maxAzs: props.vpc.maxAzs,
+        natGateways: props.vpc.natGateways,
+        subnetConfiguration: [
+            {
+                cidrMask: 24,
+                name: 'public',
+                subnetType: ec2.SubnetType.PUBLIC,
+            },
+            {
+                cidrMask: 24,
+                name: 'private',
+                subnetType: ec2.SubnetType.PRIVATE,
+            },
+            {
+                cidrMask: 24,
+                name: 'isolated',
+                subnetType: ec2.SubnetType.ISOLATED,
+            }
         ]
-      }
-    );
+    });
   }
 }
 
